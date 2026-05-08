3bfc624061036d4f50483a9fcde8399807603882
announcerf <tasaneono22@gmail.com>
2026-05-08 17:02:22 +0300

    rename

--- src/randomized-bst.ts
+++ src/randomized-bst.ts
@@ -2,19 +2,19 @@ import { BinarySearch, Node } from './binary-search';
 import { Student } from './student';
 
 export class RandomizedBST extends BinarySearch {
-  insertTreap(node: Node | null, student: Student): Node {
+  insert(node: Node | null, student: Student): Node {
     if (node === null) {
       return new Node(student);
     }
 
     if (student.lastName < node.student.lastName) {
-      node.left = this.insertTreap(node.left, student);
+      node.left = this.insert(node.left, student);
 
       if (node.left !== null && node.left.priority > node.priority) {
         node = this.rotateRight(node);
       }
     } else if (student.lastName > node.student.lastName) {
-      node.right = this.insertTreap(node.right, student);
+      node.right = this.insert(node.right, student);
 
       if (node.right !== null && node.right.priority > node.priority) {
         node = this.rotateLeft(node);
@@ -25,6 +25,6 @@ export class RandomizedBST extends BinarySearch {
   }
 
   override add(student: Student): void {
-    this.root = this.insertTreap(this.root, student);
+    this.root = this.insert(this.root, student);
   }
 }
